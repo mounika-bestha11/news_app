506e87bcca96ea836943aee2e09efaabf9d2fa07
FlutLab.io <>
2022-10-01 06:41:27 +0000

    base_update

---
+++ lib/screens/feed_screens.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+
+import '../post_model.dart';
+import 'view_post_screens.dart';
+
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  Widget _buildPost(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Container(
+        width: double.infinity,
+        height: 560.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        child: ClipOval(
+                          child: Image(
+                            height: 50.0,
+                            width: 50.0,
+                            image: AssetImage(posts[index].authorImageUrl),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      posts[index].authorName,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(posts[index].timeAgo),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.more_horiz),
+                      color: Colors.black,
+                      onPressed: () => print('More'),
+                    ),
+                  ),
+                  InkWell(
+                    onDoubleTap: () => print('Like post'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ViewPostScreen(
+                            post: posts[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(10.0),
+                      width: double.infinity,
+                      height: 400.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        boxShadow: [
+                          const BoxShadow(
+                            color: Colors.black45,
+                            offset: Offset(0, 5),
+                            blurRadius: 8.0,
+                          ),
+                        ],
+                        image: DecorationImage(
+                          image: AssetImage(posts[index].imageUrl),
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                IconButton(
+                                  icon: const Icon(Icons.favorite_border),
+                                  iconSize: 30.0,
+                                  onPressed: () => print('Like post'),
+                                ),
+                                const Text(
+                                  '2,515',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(width: 20.0),
+                            Row(
+                              children: <Widget>[
+                                IconButton(
+                                  icon: const Icon(Icons.chat),
+                                  iconSize: 30.0,
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => ViewPostScreen(
+                                          post: posts[index],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                                const Text(
+                                  '350',
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.bookmark_border),
+                          iconSize: 30.0,
+                          onPressed: () => print('Save post'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEDF0F6),
+      body: ListView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const Text(
+                  'Instagram',
+                  style: TextStyle(
+                    fontFamily: 'Billabong',
+                    fontSize: 32.0,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.live_tv),
+                      iconSize: 30.0,
+                      onPressed: () => print('IGTV'),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Container(
+                      width: 35.0,
+                      child: IconButton(
+                        icon: const Icon(Icons.send),
+                        iconSize: 30.0,
+                        onPressed: () => print('Direct Messages'),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 100.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: stories.length + 1,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return const SizedBox(width: 10.0);
+                }
+                return Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black45,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        height: 60.0,
+                        width: 60.0,
+                        image: AssetImage(stories[index - 1]),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          _buildPost(0),
+          _buildPost(1),
+        ],
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: const Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.dashboard,
+                size: 30.0,
+                color: Colors.black,
+              ),
+              label: 'Lets do',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 30.0,
+                color: Colors.grey,
+              ),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                child: FlatButton(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  color: const Color(0xFF23B66F),
+                  onPressed: () => print('Upload photo'),
+                  child: const Icon(
+                    Icons.add,
+                    size: 35.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              label: 'Lets do',
+            ),
+            const BottomNavigationBarItem(
+              icon: const Icon(
+                Icons.favorite_border,
+                size: 30.0,
+                color: Colors.grey,
+              ),
+              label: 'Lets do',
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+                size: 30.0,
+                color: Colors.grey,
+              ),
+              label: 'Lets do',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
